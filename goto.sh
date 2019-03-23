a3db8d1a2f65bb710090149f7a1e541317469421
huqingkun <huqingkun@gmail.com>
2019-03-23 19:49:14 +0800

    auto load all ssh statement in current shell history and store to .server-list file

--- goto.sh
+++ goto.sh
@@ -1,9 +1,33 @@
 LIST_DIR=`dirname $0`
+SERVER_LIST_FILE="${LIST_DIR}/.server-list"
+
+function init()
+{
+    echo "Start init method"
+    echo $SHELL
+    shname=$(echo $SHELL | awk -F/ '{print $NF}')
+    awk -v file=${SERVER_LIST_FILE} -F' ' '{if($1=="ssh") print ":"$2 > file}' ~/.${shname}_history
+    sort ${SERVER_LIST_FILE} | uniq | tee ${SERVER_LIST_FILE}
+    echo "End init method"
+}
+
+if [[ -f "${SERVER_LIST_FILE}" ]]
+then
+    echo ${SERVER_LIST_FILE}
+else
+    touch "${SERVER_LIST_FILE}"
+    init
+fi
+
 case "$1" in
     -l | --list)
-    cat $LIST_DIR/server-list.txt
+    cat ${SERVER_LIST_FILE}
     ;;
     * )
-    ssh $(cat ${LIST_DIR}/server-list.txt | grep $1 | cut -d : -f 2)
+    svc=$(cat ${SERVER_LIST_FILE} | grep $1 | cut -d : -f 2)
+    svc=$(echo $svc | cut -d ' ' -f 1)
+    echo $svc
+    ssh $svc
     ;;
 esac
+
